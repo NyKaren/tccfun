8fc403023e6c6e4878fb65808cc4c423813f6803
Karen <12010728+NyKaren@users.noreply.github.com>
2022-05-22 15:21:21 -0300

    Adding page not found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import EditUser from "./Pages/EditUser";
 import AddUser from "./Pages/AddUser";
 import UserTable from "./Pages/UserTable";
 import ManageActivity from "./Pages/ManageActivity";
 import ActivityTable from "./Pages/ActivityTable";
 import NavBar from "./Components/NavBar";
+import NotFoundPage from './Components/NotFoundPage';
 
 class App extends Component {
   render() {
@@ -19,6 +20,8 @@ class App extends Component {
               <Route path="/userTableView" component={UserTable} />
               <Route path="/editUser" component={EditUser} />
               <Route path="/logout" component={UserTable} />
+              <Route path="/404" component={NotFoundPage} />
+              <Redirect to="/404" />
           </Switch>
       </Router>
     );
---
+++ frontend/src/Components/NotFoundPage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {makeStyles} from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+const useStyles = makeStyles(theme => ({
+    table: {
+        minWidth: 600
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main
+    },
+    paper: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: `10px`,
+        height: "100%",
+        width: "99%",
+        marginTop: theme.spacing(7)
+    },
+    link: {
+        color: "rgba(0,0,0,0.65)",
+        textDecoration: "none",
+        marginLeft: "10%",
+        alignSelf: "flex-start",
+        "&:hover": {
+            color: "rgba(0,0,0,1)"
+        }
+    }
+}));
+
+export default function NotFoundPage() {
+    const classes = useStyles();
+    return (
+        <div className={classes.paper}>
+            <Typography component="h1" variant="h5">
+                404 Page not found
+            </Typography>
+            <div>
+                <p style={{textAlign:"center"}}>
+                    <Link to="/">Ir para tela inicial. </Link>
+                </p>
+            </div>
+        </div>
+    );
+}
